c4cb509ae9b98bb374438d3b4f8e403e23b603fc
David J. Malan <malan@harvard.edu>
2018-10-09 00:15:55 -0400

    added webpack example

---
+++ todo6/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div id="app">
+        <form v-on:submit.prevent="add">
+            <input placeholder="TODO" type="text" v-model="todo">
+            <input type="submit" value="Add">
+        </form>
+        <todo-list v-bind:todos="filtered"></todo-list>
+        <input placeholder="Filter" type="text" v-cloak v-show="todos.length" v-model="filter">
+    </div>
+
+</template>
+
+<script>
+
+    import TodoList from './TodoList.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            'todo-list': TodoList
+        },
+        data() {
+            return {
+                filter: '',
+                todo: '',
+                todos: []
+            }
+        },
+        computed: {
+            filtered() {
+                return this.todos.filter(todo => todo.indexOf(this.filter) > -1);
+            }
+        },
+        methods: {
+            add() {
+                this.todos.push(this.todo);
+                this.todo = '';
+            }
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
---
+++ todo6/src/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <ul>
+        <li v-for="(todo, index) in todos">
+            {{ todo }}
+        </li>
+    </ul>
+
+</template>
+
+<script>
+
+    export default {
+        props: {
+            todos: Array
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
